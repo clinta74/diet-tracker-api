1d56266a3bacfe1d9503cf583f36c64fa2760c18
Clinton Andrews <clint@pollyspeople.net>
2021-05-11 11:03:13 -0400

    update tracking

--- DataLayer/Models/UserDailyTracking.cs
+++ DataLayer/Models/UserDailyTracking.cs
@@ -9,8 +9,9 @@ namespace diet_tracker_api.DataLayer.Models
         [Key]
         public int UserDailyTrackingId { get; set; }
         public string UserId { get; set; }
-        [Column(TypeName = "date")]
         public int UserTrackingId { get; set; }
+
+        [Column(TypeName = "date")]
         public DateTime Day { get; set; }
         public int Value { get; set; }
         public DateTime When { get; set; }
---
+++ Migrations/20210511150227_update-trackings.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace diet_tracker_api.Migrations
+{
+    public partial class updatetrackings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "UserDailyTracking");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserTrackingId",
+                table: "UserDailyTracking",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserDailyTracking_UserTrackingId",
+                table: "UserDailyTracking",
+                column: "UserTrackingId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserDailyTracking_UserTracking_UserTrackingId",
+                table: "UserDailyTracking",
+                column: "UserTrackingId",
+                principalTable: "UserTracking",
+                principalColumn: "UserTrackingId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserDailyTracking_UserTracking_UserTrackingId",
+                table: "UserDailyTracking");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserDailyTracking_UserTrackingId",
+                table: "UserDailyTracking");
+
+            migrationBuilder.DropColumn(
+                name: "UserTrackingId",
+                table: "UserDailyTracking");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "UserDailyTracking",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
