c2e13752676ea480d8ab074061d7acd6c266cc40
Clinton Andrews <clint@pollyspeople.net>
2021-05-27 11:38:19 -0400

    fix get user id

--- Extensions/HttpContextExtensions.cs
+++ Extensions/HttpContextExtensions.cs
@@ -4,6 +4,6 @@ namespace diet_tracker_api.Extensions
 {
     public static class HttpContextExtensions
     {
-        public static string GetUserId(this HttpContext context) => context.GetUserId();
+        public static string GetUserId(this HttpContext context) => context.User.Identity.Name;
     }
 }
